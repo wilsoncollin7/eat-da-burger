fe3f0a268fe5c962ff7427ef9bb963c123ded6ff
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-09-24 15:02:27 -0400

    all styling done for now, all functions work properly!

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,12 @@
+USE eat_da_burger;
+
 INSERT INTO burgers (name) VALUES ("Cheese Burger");
 INSERT INTO burgers (name) VALUES ("Bacon Cheese Burger");
 INSERT INTO burgers (name) VALUES ("Chicken Burger");
 INSERT INTO burgers (name) VALUES ("Lamb Burger");
 INSERT INTO burgers (name) VALUES ("Guacamole Burger");
 INSERT INTO burgers (name) VALUES ("Double Cheese Burger");
-INSERT INTO burgers (name) VALUES ("Vegan Burger");
-INSERT INTO burgers (name) VALUES ("Southwest Burger");
+INSERT INTO burgers (name, devoured) VALUES ("Vegan Burger", true);
+INSERT INTO burgers (name, devoured) VALUES ("Southwest Burger", true);
+
+SELECT * FROM burgers;
