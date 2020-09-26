6b35447930a699b3e3bf4da83dc1aef9b7d7df75
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-09-26 12:05:30 -0400

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,18 @@
 // =============================================================
 const mysql = require("mysql");
 
-const connection = connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "dean",
-    database: "eat_da_burger"
-});
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }
+  else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "dean",
+      database: "burgers_db"
+    });
+  }
 
 connection.connect((err) => {
     if (err) throw err;
