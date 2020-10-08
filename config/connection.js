9774032b9c4074d963263d01aaadf01a25d55321
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-10-08 16:00:54 -0400

    changed the connection.js again

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,17 @@
 // =============================================================
 const mysql = require("mysql");
 
-
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "dean",
-  database: "eat_da_burger"
-});
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }
+  else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "dean",
+      database: "eat_da_burger"
+    });
 
 
 connection.connect((err) => {
