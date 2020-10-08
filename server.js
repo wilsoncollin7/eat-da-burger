fbe2a56bef39011b631529de95251e47529c5a58
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-10-08 16:25:29 -0400

    changed the connection.js again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // =============================================================
 const express = require("express");
 
-let PORT = process.env.port || 8080;
+const PORT = process.env.port || 8080;
 
 const app = express();
 
