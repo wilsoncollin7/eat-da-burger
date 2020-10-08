2b0984b1a7be6b2d36263ea55b8582d23a2f1a2f
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-10-08 16:21:07 -0400

    changed the connection.js again

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,8 @@
 // =============================================================
 const mysql = require("mysql");
 
+let connection;
+
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
   }
